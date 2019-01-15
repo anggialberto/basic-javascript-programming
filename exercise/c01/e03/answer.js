9b08610ef40b6b1fe8e660906cb8cd635eef56bd
anggialberto <albertoanggi@gmail.com>
2019-01-16 01:49:56 +0700

    Create answer.js

---
+++ exercise/c01/e03/answer.js
@@ -0,0 +1,46 @@
+var tanggal = 15;
+var bulan = 5;
+var tahun = 1998;
+
+
+switch(bulan) {
+  case 1:
+    console.log(tanggal + ' ' + 'Januari' + ' ' + tahun);
+  break;
+  case 2:
+    console.log(tanggal + ' ' + 'Februari' + ' ' + tahun);
+  break;
+  case 3:
+    console.log(tanggal + ' ' + 'Maret' + ' ' + tahun);
+  break;
+  case 4:
+    console.log(tanggal + ' ' + 'April' + ' ' + tahun);
+  break;
+  case 5:
+    console.log(tanggal + ' ' + 'Mei' + ' ' + tahun);
+  break;
+  case 6:
+    console.log(tanggal + ' ' + 'Juni' + ' ' + tahun);
+  break;
+  case 7:
+    console.log(tanggal + ' ' + 'Juli' + ' ' + tahun);
+  break;
+  case 8:
+    console.log(tanggal + ' ' + 'Agustus' + ' ' + tahun);
+  break;
+  case 9:
+    console.log(tanggal + ' ' + 'Sebtember' + ' ' + tahun);
+  break;
+  case 10:
+    console.log(tanggal + ' ' + 'Oktober' + ' ' + tahun);
+  break;
+  case 11:
+    console.log(tanggal + ' ' + 'November' + ' ' + tahun);
+  break;
+  case 12:
+    console.log(tanggal + ' ' + 'Desember' + ' ' + tahun);
+  break;
+  default:
+    console.log("Tidak terdapat bulan lebih dari 12");
+  break;
+} 
