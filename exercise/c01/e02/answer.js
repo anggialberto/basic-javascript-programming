d9c5f2875b2a9b8d868d0d6e26412fb22814e4f1
anggialberto <albertoanggi@gmail.com>
2019-01-16 01:42:16 +0700

    Update answer.js

--- exercise/c01/e02/answer.js
+++ exercise/c01/e02/answer.js
@@ -2,13 +2,16 @@
 
 if (peran === 'Ksatria') {
   var tugas = "kamu dapat menyerang dengan senjatamu!";
+  console.log("Selamat datang di Dunia Proxytia, " + nama);
+  console.log("Halo " + peran + " "+ nama + ", " + tugas);
 } else if (peran === 'Tabib') {
   var tugas = "kamu akan membantu temanmu yang terluka."
+  console.log("Selamat datang di Dunia Proxytia, " + nama);
+  console.log("Halo " + peran + " "+ nama + ", " + tugas);
 } else if (peran === 'Penyihir') {
   var tugas = "ciptakan keajaiban yang membantu kemenanganmu!";
+  console.log("Selamat datang di Dunia Proxytia, " + nama);
+  console.log("Halo " + peran + " "+ nama + ", " + tugas);
 } else {
  console.log("Halo " + nama + ", Pilih peranmu untuk memulai game!");
- throw("");
 }
-console.log("Selamat datang di Dunia Proxytia, " + nama);
-console.log("Halo " + peran + " "+ nama + ", " + tugas);
