afef6fc4fe74f5794bb348206cc662a4e22d6a60
anggialberto <albertoanggi@gmail.com>
2019-01-15 22:06:12 +0700

    Create solution.js

---
+++ exercise/c01/e01/solution.js
@@ -0,0 +1,13 @@
+var number1 = 33;
+var number2 = 66;
+var string1 = "foo";
+var string2 = "bar";
+var boolean1 = true;
+var boolean2 = false;
+
+console.log(number1);
+console.log(number2);
+console.log(string1);
+console.log(string2);
+console.log(boolean1);
+console.log(boolean2);
