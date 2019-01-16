54c1cadb9a084709f9c8c926998b986433c73491
anggialberto <albertoanggi@gmail.com>
2019-01-16 22:51:24 +0700

    Create answer.js

---
+++ exercise/c04/e02/answer.js
@@ -0,0 +1,10 @@
+function cariPelaku(str) {
+  return str.match(/abc/g).length;
+}
+
+// TEST CASES
+console.log(cariPelaku('mabcvabc')); // 2
+console.log(cariPelaku('abcdabdc')); // 1
+console.log(cariPelaku('bcabcac')); // 1
+console.log(cariPelaku('abcabcabc')); // 3
+console.log(cariPelaku('babcbacabc')); // 2
