7b10acf3c2ecc29d17720a19a454769c4ed9e394
anggialberto <albertoanggi@gmail.com>
2019-01-16 16:03:48 +0700

    Create answer_2.js

---
+++ exercise/c01/e07/answer_2.js
@@ -0,0 +1,9 @@
+function calculateMultiply(num1, num2) {
+  return num1 * num2;
+}
+
+var num1 = 5;
+var num2 = 6;
+
+var hasilPerkalian = calculateMultiply(num1,num2);
+console.log(hasilPerkalian); // Menampilkan angka 30
