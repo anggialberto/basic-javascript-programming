5e6fc4a93d1929a85c2b40da23145aec18bf7336
anggialberto <albertoanggi@gmail.com>
2019-01-16 22:09:34 +0700

    Create answer.js

---
+++ exercise/c02/e13/answer.js
@@ -0,0 +1,10 @@
+function targetTerdekat(arr) {
+  return arr.indexOf('x') - arr.indexOf('o') > 0 ? arr.indexOf('x') - arr.indexOf('o') : (arr.length-1) + (arr.indexOf('x') - arr.indexOf('o'));
+}
+
+// TEST CASES
+console.log(targetTerdekat(['', '', 'o', '', '', 'x', '', 'x'])); // 3
+console.log(targetTerdekat(['o', '', '', '', 'x', 'x', 'x'])); // 4
+console.log(targetTerdekat(['x', '', '', '', 'x', 'x', 'o', ''])); // 1
+console.log(targetTerdekat(['', '', 'o', ''])); // 0
+console.log(targetTerdekat(['', 'o', '', 'x', 'x', '', '', 'x'])); // 2
