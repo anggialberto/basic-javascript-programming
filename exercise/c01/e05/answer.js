d29cba1a4a5219db1cf53bc1231e7f1d0a4f80b0
anggialberto <albertoanggi@gmail.com>
2019-01-16 14:37:51 +0700

    Create answer.js

---
+++ exercise/c01/e05/answer.js
@@ -0,0 +1,8 @@
+console.log("LOOPING PERTAMA");
+for(var i = 1; i <= 20; i++) {
+  console.log(i + " - I love coding");
+}
+console.log("LOOPING KEDUA");
+for(var i = 20; i >= 1; i--) {
+  console.log(i + " - I will become fullstack developer");
+}
