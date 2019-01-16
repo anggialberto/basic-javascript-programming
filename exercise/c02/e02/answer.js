3095c0ac1b5c22c4af743da4cc25545286941d01
anggialberto <albertoanggi@gmail.com>
2019-01-16 21:50:44 +0700

    Create answer.js

---
+++ exercise/c02/e02/answer.js
@@ -0,0 +1,10 @@
+function balikKata(kata) {
+  return kata.split("").reverse().join("");
+}
+
+// TEST CASES
+console.log(balikKata('Hello World and Coders')); // sredoC dna dlroW olleH
+console.log(balikKata('John Doe')); // eoD nhoJ
+console.log(balikKata('I am a bookworm')); // mrowkoob a ma I
+console.log(balikKata('Coding is my hobby')); // ybboh ym si gnidoC
+console.log(balikKata('Super')); // repuS
