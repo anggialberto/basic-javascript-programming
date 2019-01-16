b7c18324ac80670a926632d4f2be780c940de909
anggialberto <albertoanggi@gmail.com>
2019-01-16 21:59:56 +0700

    Create answer.js

---
+++ exercise/c02/e07/answer.js
@@ -0,0 +1,11 @@
+function hitungJumlahKata(kalimat) {
+  let wordLength = kalimat.split(" ").length;
+  return wordLength;
+}
+
+// TEST CASES
+console.log(hitungJumlahKata('I have a dream')); // 4
+console.log(hitungJumlahKata('Never eat shredded wheat or cake')); // 6
+console.log(hitungJumlahKata('A song to sing')); // 4
+console.log(hitungJumlahKata('I')); // 1
+console.log(hitungJumlahKata('I believe I can code')); // 5
