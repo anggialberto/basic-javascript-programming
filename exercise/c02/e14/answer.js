da54742d201d7b5958b4e6387714696d99b4c88a
anggialberto <albertoanggi@gmail.com>
2019-02-26 00:27:41 +0700

    Create answer.js

---
+++ exercise/c02/e14/answer.js
@@ -0,0 +1,18 @@
+function angkaPrima(angka) {
+  if ( angka > 1 ) {
+  	for ( let i = 2; i < angka; i++) {
+  		if ( angka % i == 0) {
+  			return false;
+  		} 
+  	}
+  	return true;	
+  }
+  return false;
+}
+
+// TEST CASES
+console.log(angkaPrima(3)); // true
+console.log(angkaPrima(7)); // true
+console.log(angkaPrima(6)); // false
+console.log(angkaPrima(23)); // true
+console.log(angkaPrima(33)); // false
