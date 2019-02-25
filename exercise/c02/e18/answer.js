ce93a4646783d58002a523f867eec13c6d61eb45
anggialberto <albertoanggi@gmail.com>
2019-02-26 01:42:09 +0700

    Create answer.js

---
+++ exercise/c02/e18/answer.js
@@ -0,0 +1,23 @@
+function cariModus(arr) {
+  let arrModus = [], isSame = false;
+
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[i] === arr[j]) {
+        isSame = true;
+        arrModus.push(arr[i]);
+      } else {
+        isSame = false;
+      }
+    }
+  }
+
+  return (arrModus[0] === undefined || isSame) ? -1 : arrModus[0];
+}
+
+// TEST CASES
+console.log(cariModus([10, 4, 5, 2, 4])); // 4
+console.log(cariModus([5, 10, 10, 6, 5])); // 5
+console.log(cariModus([10, 3, 1, 2, 5])); // -1
+console.log(cariModus([1, 2, 3, 3, 4, 5])); // 3
+console.log(cariModus([7, 7, 7, 7, 7])); // -1
