e29e4e27e26f831f3711dc37e4c87f68a41a5502
anggialberto <albertoanggi@gmail.com>
2019-02-26 08:48:11 +0700

    Create answer.js

---
+++ exercise/c02/e25/answer.js
@@ -0,0 +1,16 @@
+function ubahHuruf(kata) {
+  let abjad = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+  let res = "";
+  for (let i = 0; i < kata.length; i++) {
+    let letter = abjad.indexOf(kata[i]) + 1;
+    res += abjad[letter];
+  }
+  return res;
+}
+
+// TEST CASES
+console.log(ubahHuruf('wow')); // xpx
+console.log(ubahHuruf('developer')); // efwfmpqfs
+console.log(ubahHuruf('javascript')); // kbwbtdsjqu
+console.log(ubahHuruf('keren')); // lfsfo
+console.log(ubahHuruf('semangat')); // tfnbohbu
