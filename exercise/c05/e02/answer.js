e4ced2a3bc008fd603cb7255a611bb1977d3b6ee
anggialberto <albertoanggi@gmail.com>
2019-02-26 09:29:01 +0700

    Create answer.js

---
+++ exercise/c05/e02/answer.js
@@ -0,0 +1,54 @@
+class Student {
+  constructor(name, age, dateOfBirth, gender, studentId, ...hobbies) {
+    this.name = name;
+    this.age = age;
+    this.dateOfBirth = dateOfBirth;
+    this.gender = gender;
+    this.studentId = studentId;
+    this.hobbies = hobbies;
+  }
+
+  setName(newName) {
+    this.name = newName;
+  }
+
+  setAge(newAge) {
+    this.age = newAge;
+  }
+
+  setdateOfBirth(newDateOfBirth) {
+    this.dateOfBirth = newDateOfBirth;
+  }
+
+  setGender(newGender) {
+    this.gender = gender;
+  }
+
+  addHobby(newHobby) {
+    this.hobbies.push(newHobby);
+  }
+
+  removeHobby(hobby) {
+    let locHobby = this.hobbies.indexOf(hobby);
+    this.hobbies.splice(locHobby, 1);
+
+  }
+
+  getData() {
+    console.log(
+    `Student ID : ${this.studentId}
+      Fullname : ${this.name}
+      Gender : ${this.gender}
+      Date of Birth : ${this.dateOfBirth}
+      Age : ${this.age} years old
+      Hobbies : ${this.hobbies}`
+    );
+  }
+
+}
+
+let anggi = new Student("Anggi Alberto", 20, "15 Mei 1998", "Male", "50416855", "Ski", "Games");
+anggi.getData();
+anggi.addHobby("Programming");
+anggi.removeHobby("Games");
+anggi.getData();
