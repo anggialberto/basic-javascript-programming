ca7db9531e375737f63183e16d657bc5f77b1f77
anggialberto <albertoanggi@gmail.com>
2019-02-26 12:07:13 +0700

    Create answer.js

---
+++ exercise/c05/e04/answer.js
@@ -0,0 +1,14 @@
+const phi = 3.14;
+const power = 2;
+let radius = 0;
+var calculateArea = function (radius) {
+  return phi * Math.pow(radius, power);
+}
+
+radius = 21;
+const area21 = calculateArea(radius);
+
+radius = 7;
+const area7 = calculateArea(radius);
+
+console.log('area with 21 radius: ' + area21 + ', and area with 7 radius: ' + area7);
